6bd3101cbaab63e834b2d5f95eda9ff9f78d803e
Supriya Das <41727446+supriyadas13@users.noreply.github.com>
2020-06-05 00:50:53 -0700

    initial commit

---
+++ src/main/java/com/supriya/JPAswapping/JpAswappingApplication.java
@@ -0,0 +1,13 @@
+package com.supriya.JPAswapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAswappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAswappingApplication.class, args);
+	}
+
+}
